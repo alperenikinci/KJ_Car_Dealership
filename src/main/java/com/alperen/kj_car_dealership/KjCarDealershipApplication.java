82c8b25f8f9eea1f41806f3256afd0963e8f73d0
alperenikinci <alperen.ikinci@hotmail.com>
2024-05-06 13:05:05 +0300

    Initial commit

---
+++ src/main/java/com/alperen/kj_car_dealership/KjCarDealershipApplication.java
@@ -0,0 +1,13 @@
+package com.alperen.kj_car_dealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjCarDealershipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KjCarDealershipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alperen/kj_car_dealership/KjCarDealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alperen.kj_car_dealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjCarDealershipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
